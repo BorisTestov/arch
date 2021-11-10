03fda95a56f3f7ea85da5d7a5b9900a844a6bbd6
BorisTestov <boris.testov@gmail.com>
2021-11-10 23:19:38 +0300

    colored output

--- archuefi1.sh
+++ archuefi1.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo 'Настройка языка'
+function print () {
+	echo -e "\033[36m"$@
+}
+
+print 'Настройка языка'
 loadkeys ru
 setfont cyr-sun16
 
-echo 'Синхронизация системных часов'
+print 'Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo 'Создание разделов'
+print 'Создание разделов'
 (
 	echo d;echo;echo d;echo;echo d;echo;
 	
@@ -26,12 +30,12 @@ echo 'Создание разделов'
 	echo w;
 ) | fdisk /dev/sda
 
-echo 'Форматирование дисков'
+print 'Форматирование дисков'
 mkfs.fat -F32 /dev/sda1 -I
 mkswap /dev/sda2 -L swap
 mkfs.ext4 /dev/sda3 -F -L root
 
-echo 'Монтирование дисков'
+print 'Монтирование дисков'
 umount -a
 swapoff /dev/sda2
 mkdir /mnt/boot
@@ -39,10 +43,10 @@ mount /dev/sda1 /mnt/boot
 swapon /dev/sda2
 mount /dev/sda3 /mnt
 
-echo 'Установка основных пакетов'
+print 'Установка основных пакетов'
 pacstrap /mnt base dhcpcd linux linux-headers which netctl inetutils base-devel wget linux-firmware neovim wpa_supplicant
 
-echo 'Настройка системы'
+print 'Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/BorisTestov/arch/master/archuefi2.sh)"
--- archuefi2.sh
+++ archuefi2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function print () {
+	echo -e "\033[36m"$@
+}
+
 function aur_install () {
   cd /home/$username
   git clone https://aur.archlinux.org/$1.git
@@ -18,126 +22,126 @@ function install() {
 read -p "Введите имя компьютера: " hostname
 read -p "Введите имя пользователя: " username
 
-echo 'Прописываем имя компьютера'
+print 'Прописываем имя компьютера'
 echo $hostname > /etc/hostname
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
-echo '3.4 Добавляем локаль системы'
+print 'Добавляем локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 
-echo 'Обновим текущую локаль системы'
+print 'Обновим текущую локаль системы'
 locale-gen
 
-echo 'Указываем язык системы'
+print 'Указываем язык системы'
 echo 'LANG="en_US.UTF-8"' > /etc/locale.conf
 
-echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+print 'Вписываем KEYMAP=ru FONT=cyr-sun16'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-echo 'Создадим загрузочный RAM диск'
+print 'Создадим загрузочный RAM диск'
 mkinitcpio -p linux
 
-echo '3.5 Устанавливаем загрузчик'
+print 'Устанавливаем загрузчик'
 installyy
 install grub efibootmgr 
 grub-install /dev/sda
 
-echo 'Обновляем grub.cfg'
+print 'Обновляем grub.cfg'
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo 'Ставим программу для Wi-fi'
+print 'Ставим программу для Wi-fi'
 install dialog wpa_supplicant 
 
-echo 'Добавляем пользователя'
+print 'Добавляем пользователя'
 useradd -m -g users -G wheel -s /bin/bash $username
 
-echo 'Создаем root пароль'
+print 'Создаем root пароль'
 passwd
 
-echo 'Устанавливаем пароль пользователя'
+print 'Устанавливаем пароль пользователя'
 passwd $username
 
-echo 'Устанавливаем SUDO'
+print 'Устанавливаем SUDO'
 sed -i "s/# %wheel/%wheel/g" /etc/sudoers
 
-echo 'Ставим иксы и драйвера'
+print 'Ставим иксы и драйвера'
 install xorg-server xorg-drivers xorg-xinit
 
-echo "Ставим XFCE"
+print 'Ставим XFCE'
 install xfce4 xfce4-goodies
 
-echo 'Ставим шрифты'
+print 'Ставим шрифты'
 install ttf-liberation ttf-dejavu ttf-font-awesome
 
-echo 'Ставим сеть'
+print 'Ставим сеть'
 install networkmanager network-manager-applet ppp networkmanager-openvpn
 systemctl enable NetworkManager
 systemctl enable dhcpcd
 
-echo 'Ставим аудио'
+print 'Ставим аудио'
 install pulseaudio pulseaudio-bluetooth alsa-utils alsa-lib pulseaudio-equalizer-ladspa pavucontrol blueman
 systemctl enable bluetooth
 
-echo 'Ставим пакеты для работы с fat / ntfs'
+print 'Ставим пакеты для работы с fat / ntfs'
 install exfat-utils ntfs-3g
 
-echo 'Установка архиваторов'
+print 'Установка архиваторов'
 install file-roller p7zip unrar unzip unace
 
-echo 'Установка дополнительных пакетов'
+print 'Установка дополнительных пакетов'
 install htop spectacle neofetch openssh
 systemctl enable sshd 
 
-echo 'Установка AUR (yay) и доппакетов'
+print 'Установка AUR (yay) и доппакетов'
 aur_install yay
 aur_install rtl8812au-dkms-git
 aur_install polybar
 aur_install ttf-weather-icons
 aur_install ttf-clear-sans
 
-echo 'Установка i3'
+print 'Установка i3'
 install i3-wm dmenu 
 wget https://github.com/BorisTestov/arch/raw/master/attach/config_i3wm.tar.gz
 rm -rf ~/.config/i3/*
 rm -rf ~/.config/polybar/*
 tar -xzf config_i3wm.tar.gz -C ~/
 
-echo 'Автовход в систему'
+print 'Автовход в систему'
 cp /etc/X11/xinit/xserverrc ~/.xserverrc
 wget https://raw.githubusercontent.com/BorisTestov/arch/master/attach/.xinitrc
 mv -f .xinitrc ~/.xinitrc
 mkdir /etc/systemd/system/getty@tty1.service.d/
 echo -e '[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin' "$username" '--noclear %I $TERM' > /etc/systemd/system/getty@tty1.service.d/override.conf
 
-echo 'Скачивание bashrc'
+print 'Скачивание bashrc'
 wget https://raw.githubusercontent.com/BorisTestov/arch/master/attach/.bashrc
 rm ~/.bashrc
 mv -f .bashrc ~/.bashrc
 
-echo 'Скачивание grub.cfg'
+print 'Скачивание grub.cfg'
 wget https://raw.githubusercontent.com/BorisTestov/arch/master/attach/grub
 mv -f grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo 'Ставим docker'
+print 'Ставим docker'
 install docker
 systemctl enable docker
 groupadd docker
 usermod -aG docker $username
 
-echo 'Ставим discord'
+print 'Ставим discord'
 install discord
 
-echo 'Ставим telegram'
+print 'Ставим telegram'
 install telegram-desktop
 
-echo 'Ставим браузер'
+print 'Ставим браузер'
 install vivaldi
 
-echo 'Ставим timeshift'
+print 'Ставим timeshift'
 aur_install timeshift
 
-echo 'Установка завершена!'
+print 'Установка завершена!'
 read -p "Нажмите Enter для перезагруки"
 reboot
